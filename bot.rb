777adcf209517a000a81cf08a5ea3ad5be7065ad
Joe Parrinello <joe.parrinello@gmail.com>
2016-04-14 15:29:25 -0400

    takes environment variable name optional for botname

--- bot.rb
+++ bot.rb
@@ -10,7 +10,7 @@ bot = Cinch::Bot.new do
         raise "Environment Variable for Channel not Set." unless ENV.has_key?("channel")
         c.server   = ENV['server']
         c.channels = [ENV['channel']]
-        c.nick = "NeatBot"
+        c.nick = ENV['neatbot_name'] ||"NeatBot"
     end
 
     on :channel, /\b[Nn][Ee][Aa][Tt]\b/ do |m|
